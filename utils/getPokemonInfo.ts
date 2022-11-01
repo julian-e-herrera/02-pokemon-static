038f7f74978c75d91b38dd08e30530ea95d31530
Julian Herrera <julianeherreraok@gmail.com>
2022-11-01 11:37:07 -0300

    isg

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -2,11 +2,17 @@ import { pokeApi } from '../api'
 import { Pokemon } from '../interfaces'
 
 export const getPokemonInfo = async (nameOrId: string) => {
-  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrId}`)
 
-  return {
-    sprites: data.sprites,
-    id: data.id,
-    name: data.name,
+  try {
+    
+    const { data } = await pokeApi.get<Pokemon>(`/pokemon/${nameOrId}`)
+    return {
+      sprites: data.sprites,
+      id: data.id,
+      name: data.name,
+    }
+  } catch (error) {
+    return null
   }
+
 }
